ffc6d492b4120cb55f6e70c52984a407a1d844f1
Tabata Melo <tabatamello22@gmail.com>
2019-03-13 19:37:07 -0300

    initial commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+        <h2>Nossa História</h2>
+        <p>Projeto criado para fins educativos, com o auxilio do <b>Cod3r Cursos</b> onde posso estudar e aplicar meus conhecimentos em React!!</p>
+    </div>
+)
---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
+export default props => {
+    const keyHandler = (e) => {
+        if (e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
+    return (
+        <div role='form' className='todoForm' style={{display:'flex'}} >
+                <input id='description' className='form-control'
+                    style={{width: '87%',marginRight: '10px'}}
+                    placeholder='Adicione uma tarefa' 
+                    onChange={props.handleChange}
+                    onKeyUp={keyHandler}
+                    value={props.description}></input>
+                <IconButton style='primary' icon='plus'
+                    onClick={props.handleAdd}></IconButton>
+                <IconButton style='info' icon='search'
+                    onClick={props.handleSearch}></IconButton>
+                <IconButton style='default' icon='close'
+                    onClick={props.handleClear}></IconButton>
+        </div>
+    )
+}
